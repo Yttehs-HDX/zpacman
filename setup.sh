be156dae0385ba1c1ce92dd5fb0b569b6b081de2
Yttehs <shetty.yttehs@outlook.com>
2024-05-10 23:50:47 +0800

    Better p10k theme config

--- setup.sh
+++ setup.sh
@@ -10,6 +10,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 read -p "Overwrite ~/.zshrc? [Y/n] " answer
 if [ "$answer" != "N" ] && [ "$answer" != "n" ]; then
 	cp $ZPACMAN/.zshrc ~/.zshrc
+else
+	echo "source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme" >> ~/.zshrc
 fi
 
 $ZPACMAN/bin/zpacman reload
