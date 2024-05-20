1bbe367e4cb3bccbc127cd34028c07565bb20775
Yttehs <shetty.yttehs@outlook.com>
2024-05-20 14:17:59 +0800

    Add install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ZPACMAN=~/.zpacman
+
+if [ -d $ZPACMAN ]; then
+	echo -e "\e[0;31mzpacman is already installed, updating...\e[0m"
+	cd $ZPACMAN && git pull
+	$ZPACMAN/bin/zpacman reload
+else
+	git clone	https://github.com/Yttehs-HDX/zpacman.git --depth=1 $ZPACMAN
+	echo -e "\e[1;32mzpacman installed successfully\e[0m"
+	cd $ZPACMAN && ./setup.sh
+fi
+
+
