b06cbbfa2189d205af3b4f544310c3ae8dfecc2b
Yttehs <shetty.yttehs@outlook.com>
2024-10-24 12:46:28 +0800

    feat: set default configs

--- config/plugins-example.zsh
+++ config/plugins-example.zsh
@@ -4,45 +4,4 @@
 #
 
 plugins=(
-	archlinux
-	autojump
-	colored-man-pages
-	command-not-found
-	copypath copyfile
-	cp
-	docker
-	dotnet
-	emoji
-	emoji-clock
-	extract
-	eza
-	fzf
-	gh
-	git
-	git-lfs
-	github
-	gitignore
-	golang
-	gradle
-	history-substring-search
-	kate
-	nmap
-	npm
-	python
-	rand-quote
-	rust
-	safe-paste
-	ssh
-	sudo
-	tmux
-	ufw
-	vscode
-	web-search
-	z
-	zsh-autosuggestions	# https://github.com/zsh-users/zsh-autosuggestions
-	zsh-random-quotes	# https://github.com/vkolagotla/zsh-random-quotes
-	zsh-syntax-highlighting	# https://github.com/zsh-users/zsh-syntax-highlighting
-	zsh-wakatime	# https://github.com/wbingli/zsh-wakatime
-	zsh-you-should-use	# https://github.com/MichaelAquilina/zsh-you-should-use
-	zsh-zpacman	# https://github.com/Yttehs-HDX/zsh-zpacman
 )
